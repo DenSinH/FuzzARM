2c38c6ce3b3be4e5786b1d7c8598b8b3c340a61e
Matthew Berry <me@mattrb.com>
2020-10-18 13:08:13 -0700

    mark fasmarm executable, remove that part of script, ignore pycache

--- main.py
+++ main.py
@@ -20,30 +20,19 @@ executable = {
     "win32": "FASMARM.EXE",
     "linux": "fasmarm",
     "linux2": "fasmarm",
-    "darwin": "fasmarm.o"
+    "darwin": "fasmarm.o",
 }
 
 if sys.platform not in executable:
     raise OSError("OS not supported")
 
-if "linux" in sys.platform or "darwin" in sys.platform:
-    print("chmod +x ./FASMARM/fasmarm")
-    chmod = sp.run("chmod +x ./FASMARM/fasmarm")
-    if chmod.stdout:
-        print(chmod.stdout)
-    if chmod.returncode != 0:
-        print(chmod.sterr)
-        raise OSError("Could not chmod ./FASMARM/fasmarm")
-
-
 completed_process = sp.run(
     [
         f"./FASMARM/{executable[sys.platform]}",
         str(Path("./asm/main.asm").absolute()),
-        str(Path("./main.gba").absolute())
-     ]
+        str(Path("./main.gba").absolute()),
+    ]
 )
 
 if completed_process.returncode != 0 or completed_process.stderr:
     raise Exception(completed_process.stderr)
-
